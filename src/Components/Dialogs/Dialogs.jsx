3ed9be671bba40be2fc04d52de8171a572c627a4
Yuriy <dementev.yuriy@list.ru>
2022-08-03 11:40:19 +0300

    добавил map функцию

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -15,8 +15,7 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
-  
-  let dialogsData = [
+  let dialogs = [
     { id: 1, name: "Yuriy" },
     { id: 2, name: "Nika" },
     { id: 3, name: "Liza" },
@@ -24,22 +23,25 @@ const Dialogs = (props) => {
     { id: 5, name: "David" },
   ];
 
-let messagesData = [
-  {id : 1, message : 'Hi'},
-  {id : 2, message : 'How is your dream ?'},
-  {id : 3, message : 'Nice bro! Thanks ?'},
-]
+  let messages = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How is your dream ?" },
+    { id: 3, message: "Nice bro! Thanks ?" },
+  ];
+
+  let dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+
+  let messagesElements = messages.map((m) => <Message message={m.message} />);
 
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        {/* <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} /> */}
+        {dialogsElements}
       </div>
       <div className={classes.messages}>
-        {/* <Message message={messagesData[0].message} />
-        <Message message={messagesData[1].message} />
-        <Message message={messagesData[2].message}/> */}
+        {messagesElements}
       </div>
     </div>
   );
--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -4,10 +4,13 @@ import classes from "./../MyPosts/MyPosts.module.css";
 
 const MyPosts = (props) => {
 
-  let postData = [
+  let posts = [
     {id : 1, post : "Hi, how are you?", likesCount : 4},
     {id : 1, post : "It's my first post", likesCount: 54}
   ]
+
+let postsElements = posts
+.map( p =>  <Post message={p.post} likesCount={p.likesCount} />)
 
   return (
     <div className={classes.postsBlock}>
@@ -19,8 +22,7 @@ const MyPosts = (props) => {
         <button>Add post</button>
       </div>
       <div className={classes.posts}></div>
-      <Post message={postData[0].post} likesCount={postData[0].likesCount} />
-      <Post message={postData[1].post} likesCount={postData[1].likesCount} />
+      {postsElements}
     </div>
   );
 };
