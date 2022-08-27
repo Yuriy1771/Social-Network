ea47134b0415d5f0ac45defd225b06d5218e13b8
Yuriy <dementev.yuriy@list.ru>
2022-08-27 10:36:32 +0300

    добавил возможность добавить пост на стену в профиле

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,3 +1,5 @@
+import {rerenderEntireTree} from '../render';
+
 let state = {
   profilePage: {
     posts: [
@@ -147,6 +149,7 @@ export let addPost = (postMessage) => {
     likesCount : 0,
   }
   state.profilePage.posts.push(newPost);
+  rerenderEntireTree(state);
 };
 
 export default state;
