37a79cb9e34ad72868d977a67b367d76a8270b20
Yuriy <dementev.yuriy@list.ru>
2022-08-31 11:52:08 +0300

    небольшие правки дизайна

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import classes from './Header.module.css';
+import logo from './imgProject/LogoSocialNetwork.png';
 
 const Header = () => {
     return (
         <header className={classes.header}>
-        <img src="https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/4a14e7b2de7f6eaf5a6c98cb8c00b8de.png" alt="picture" />
+        <img src={logo} alt="picture" />
       </header>
     )
 }
 
 
 export default Header;
+
+// https://cdn.shopify.com/shopifycloud/hatchful_web_two/bundles/4a14e7b2de7f6eaf5a6c98cb8c00b8de.png
